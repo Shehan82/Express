13447eaddb829cd2f11dd4d59439e01042f363bb
shehan82 <shehansandeepa82@gmail.com>
2020-11-30 22:27:05 +0530

    set static folder using app.use

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("<h1>Hello wordssdsld</h1>");
-})
+// app.get('/', (req, res)=>{
+//     // res.send("<h1>Hello wordssdsld</h1>");
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+//set static folder for urls
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , ()=>{console.log(`Server started on port ${PORT}`)});
