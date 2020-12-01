1be9badcc745fc55e23539ac2421092503b6486c
shehan82 <shehansandeepa82@gmail.com>
2020-12-01 14:58:53 +0530

    create update function using router.get

--- routes/api/students.js
+++ routes/api/students.js
@@ -21,15 +21,48 @@ router.get('/:name', (req, res)=>{
 })
 
 router.post('/', (req, res)=>{
-    
-    const newMember = {
+
+    const newStudent = {
         name: req.body.name,
         age: req.body.age,
         uni: req.body.uni
     }
 
-    students.push(newMember);
+    students.push(newStudent);
     res.json(students);
 })
+
+
+//update
+router.put('/:name', (req, res)=>{
+    const found = students.some((student)=>{return student.name === req.params.name});
+    if(found)
+    {
+        const updtdMember = req.body;
+        students.map(member => {
+            if(member.name === req.params.name)
+            {
+                member.name = updtdMember.name;
+                member.age = updtdMember.age;
+                member.uni = updtdMember.uni;
+                res.json(member);
+               
+            }
+            else
+            {
+                res.json({msg:"does not find any matching"})
+            }
+    })
+
+        
+    }
+    else
+    {
+        res.status(400);
+        res.json({msg:"bad request"});
+    }
+    
+})
+
 
 module.exports = router;
