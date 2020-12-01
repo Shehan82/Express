2a45b3ab64fcf8503f2967ea6f08043f13244756
shehan82 <shehansandeepa82@gmail.com>
2020-12-01 12:18:06 +0530

    create a simple API and get data from using postman

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
 const students = require('./Students');
-const moment = require('moment');
 const logger = require('./middleware/logger');
 
 const app = express();
-
-
-
 
 app.use(logger);
 
@@ -19,6 +15,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/students', (req, res)=>{
     res.json(students);
 });
+
+app.get('/api/students/:name', (req, res)=>{
+    const found = students.some((student)=>{return student.name === req.params.name});
+    if(found)
+    {
+        res.json(students.filter(student => student.name === req.params.name))
+    }
+    else
+    {
+        res.status(400);
+        res.json({msg:"bad request"});
+    }
+    
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT , ()=>{console.log(`Server started on port ${PORT}`)});
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const logger = (req,res,next)=>{
     console.log(`${req.protocol}://${req.host}${req.originalUrl} time:${moment().format()}`);
     next();
